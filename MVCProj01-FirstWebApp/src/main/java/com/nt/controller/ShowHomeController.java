043c3c7ba59c02d4125e0d3269adfad31313eaef
jhansitm <jhansi.tm@gmail.com>
2019-09-18 15:58:28 +0530

    first commit

---
+++ MVCProj01-FirstWebApp/src/main/java/com/nt/controller/ShowHomeController.java
@@ -0,0 +1,25 @@
+package com.nt.controller;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ShowHomeController  implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		Date d=null;
+		ModelAndView mav = null;
+		//get date class Object
+		d=new Date();
+		return new ModelAndView("home","sysDate",d);
+		
+		
+	}
+
+}
